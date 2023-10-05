201e01e07c888f09cc7dfdf4a5db2e92ff022d71
mariegrace <mariegracebmg@gmail.com>
2023-10-05 16:15:36 +0200

    Update the post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :author
-end
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  def update_user_post_counter
+    author.update(posts_count: auhtor.posts.count)
+  end
