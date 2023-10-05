729ac9a7bd53546993322cd5c72a48f3ff9e45df
mariegrace <mariegracebmg@gmail.com>
2023-10-05 16:19:58 +0200

    Update the comment.rb file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
+
+  after_create :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post.update(commentsCounter: post.comments.count)
+  end
 end
