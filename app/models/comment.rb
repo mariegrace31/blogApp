2e176af3acc3af9e20d63bf5e18419b18177f213
mariegrace <mariegracebmg@gmail.com>
2023-10-06 20:10:25 +0200

    Update methods files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User'
+  belongs_to :user
   belongs_to :post
 
   after_create :update_post_comments_counter
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_create :update_user_post_counter
+
+  private
 
   def update_user_post_counter
     author.update(posts_count: author.posts.count)
   end
+
+  public
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
