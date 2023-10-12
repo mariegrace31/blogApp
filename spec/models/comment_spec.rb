3891ebc3abf44ed6ea781f2d91552ec3ff4627c9
mariegrace <mariegracebmg@gmail.com>
2023-10-12 12:06:07 +0200

    Update the comment_spec.rb file

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
+
 RSpec.describe Comment, type: :model do
-  subject { Like.new(user_id: 1, post_id: 1) }
+  user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  post = Post.create(author_id: user.id, title: 'Hi', text: 'This is my post')
+  subject { Comment.new(post_id: post.id, user_id: user.id, text: 'Hi Tom!') }
   before { subject.save }
-  it 'update_comments_counter should  function' do
-    expect { subject.send(:update_post_comments_counter) }
+
+  it 'should validate it\'s text' do
+    expect(subject.text).to eq 'Hi Tom!'
+    expect(subject.post_id).to eq post.id
+    expect(subject.user_id).to eq user.id
   end
 end
