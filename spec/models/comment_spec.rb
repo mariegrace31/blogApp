c2e9efb650526009a3857e3da421edfb1a59aacb
mariegrace <mariegracebmg@gmail.com>
2023-10-12 12:30:28 +0200

    Update tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-  post = Post.create(author_id: user.id, title: 'Hi', text: 'This is my post')
+  post = Post.create(author_id: user.id, title: 'Hi', content: 'This is my post')
   subject { Comment.new(post_id: post.id, user_id: user.id, text: 'Hi Tom!') }
+
   before { subject.save }
 
   it 'should validate it\'s text' do
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
+
 RSpec.describe Like, type: :model do
-  subject { Like.new(user_id: 1, post_id: 1) }
+  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let(:post) { Post.create(author_id: user.id, title: 'Hi', content: 'This is my post') }
+  subject { Like.new(post_id: post.id, user_id: user.id) }
+
   before { subject.save }
-  it 'update_likes_counter should  function' do
-    expect { subject.send(:update_post_like_counter) }
+
+  it 'author_id is not present' do
+    subject.user_id = false
+    expect(subject).to_not be_valid
+  end
+
+  it 'post_id is not present' do
+    subject.post_id = nil
+    subject.user_id = nil
+    expect(subject).to_not be_valid
   end
 end
