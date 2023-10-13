16d93be14b40c3f5f51e897d7b6dcba2a44518b4
mariegrace <mariegracebmg@gmail.com>
2023-10-13 11:51:26 +0200

    Ad data to the data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Moha', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Yemen.', posts_counter: 0)
+second_user = User.create(name: 'Adeeb', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Saudi.', posts_counter: 0)
+
+first_post = Post.create(author: first_user, title: 'Hello', content: 'This is my first post', likes_counter: 0, comments_counter: 0)
+second_post = Post.create(author: first_user, title: 'Hi', content: 'This is my second post', likes_counter: 0, comments_counter: 0)
+third_post = Post.create(author: second_user, title: 'Me', content: 'This is my third post', likes_counter: 0, comments_counter: 0)
+fourth_post = Post.create(author: second_user, title: 'Yo!', content: 'This is my fourth post', likes_counter: 0, comments_counter: 0)
+
+Comment.create(post: first_post, user: first_user, text: 'Hi Moha!')
+Comment.create(post: second_post, user: first_user, text: 'Beautiful!')
+Comment.create(post: third_post, user: first_user, text: 'I like your post!')
+Comment.create(post: fourth_post, user: second_user, text: 'Nice post!')
+Comment.create(post: third_post, user: second_user, text: 'I am really inspired!')
+Comment.create(post: fourth_post, user: second_user, text: 'Bye!')
+Comment.create(post: fourth_post, user: first_user, text: 'Yo!')
