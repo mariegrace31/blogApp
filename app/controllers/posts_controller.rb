854b699e16225783cccd6eab9e23e35f449a928d
mariegrace <mariegracebmg@gmail.com>
2023-10-14 19:43:18 +0200

    Update the posts_controller.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,28 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(current_user)
+
+    else
+      flash[:alert] = 'Something went wrong'
+      render 'new'
+    end
+
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
