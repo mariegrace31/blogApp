0dd7b80b05a677b5c34a2b56cd9d4800bb9ae59e
mariegrace <mariegracebmg@gmail.com>
2023-10-14 19:41:37 +0200

    Add the likes_controller.rb file

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+   @post = Post.find(params[:id])
+   @like = Like.create(user: current_user, post: @post)
+
+   if @like.persisted?
+     flash[:notice] = 'You liked this post!'
+     redirect_to user_post_path(current_user, @post)
+   else
+     flash[:alert] = 'Something went wrong!'
+     render :new
+   end
+ end
+end
