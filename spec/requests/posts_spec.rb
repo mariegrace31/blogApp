b5e66f95f90f1e99cb6e08096414bbaf710ef19d
mariegrace <mariegracebmg@gmail.com>
2023-10-19 22:54:49 +0200

    Improve the tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    before do
-      get user_posts_path(user_id: 1)
-    end
-
+  before(:each) do
+    @user = User.create(name: 'Alishah', photo: 'https://placehold.co/200x133', bio: 'Teacher from US.',
+                        posts_count: 0)
+    @post = @user.posts.create(title: 'Hello', content: 'This is my first post.', commentsCounter: 0, likes_count: 0)
+  end
+  context 'GET /index' do
     it 'returns http success' do
+      get "/users/#{@user.id}/posts"
       expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
     end
 
-    it 'renders the correct template' do
+    it 'render the index template' do
+      get "/users/#{@user.id}/posts/"
       expect(response).to render_template('posts/index')
     end
 
-    it 'responds with the correct body' do
-      expect(response.body).to include('These are the Posts of the user!')
+    it 'includes the correct placeholder text' do
+      get "/users/#{@user.id}/posts/"
+      expect(response.body).to include('Number of posts:')
     end
   end
 
-  describe 'GET /show' do
-    before do
-      get user_post_path(user_id: 1, id: 1)
-    end
-
+  context 'GET /show' do
     it 'returns http success' do
+      get "/users/#{@user.id}/posts/#{@post.id}/"
       expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
     end
 
-    it 'renders the correct template' do
+    it 'render the show template' do
+      get "/users/#{@user.id}/posts/#{@post.id}/"
       expect(response).to render_template('posts/show')
-    end
-
-    it 'responds with the correct body' do
-      expect(response.body).to include('This is an individual post')
     end
   end
 end
