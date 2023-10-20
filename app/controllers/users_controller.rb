610b4002a924e52b0fab67d76af455bb24020dd7
mariegrace <mariegracebmg@gmail.com>
2023-10-20 14:12:53 +0200

    Fix the sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user = User.includes(:posts).find(params[:id])
     @posts = @user.recent_posts
   end
 
