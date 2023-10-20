dffc751f506871eca5ba1fe808bd0dd3e9465dcd
mariegrace <mariegracebmg@gmail.com>
2023-10-20 13:09:24 +0200

    Add uncormied methode to devise

---
+++ db/migrate/20231020110256_add_unconfirmed_to_devise.rb
@@ -0,0 +1,10 @@
+class AddUnconfirmedToDevise < ActiveRecord::Migration[7.0]
+  def change
+    change_table(:users) do |t|
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
+      end
+  end
+end
